aef840bbaf11cf88bdf3081515dd4c2c696b596f
RonenTGreat <ronenh53@gmail.com>
2025-12-09 18:01:37 +0000

    .

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,9 +7,15 @@
 
 @forelse ($tasks as $task)
 <div>
-    <a href="{{ route('tasks.show', ['id' => $task->id]) }}">{{ $task->title }}</a>
+    <a href="{{ route('tasks.show', ['task' => $task->id]) }}">{{ $task->title }}</a>
 </div>
 @empty
 <div>There are not task!</div>
 @endforelse
+
+@if ($task->count())
+    <div>
+        {{ $tasks->links() }}
+    </div>
+@endif
 @endsection
