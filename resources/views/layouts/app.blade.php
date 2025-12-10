fa6dcc3e45028fa90e7c7aeeb6f9d27c693b6d0f
RonenTGreat <ronenh53@gmail.com>
2025-12-10 13:24:48 +0000

    styled pages

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,14 +4,46 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Laravel Task List App</title>
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+
+    {{-- blade-formatter-disable --}}
+    <style type="text/tailwindcss">
+        .btn {
+            @apply rounded-md px-2 py-1 text-center font-medium shadow-sm ring-1 ring-slate-700 text-slate-700 hover:bg-slate-50;
+        }
+
+        .link{
+            @apply font-medium text-gray-700 underline decoration-pink-500
+        }
+
+        label {
+            @apply block uppercase text-slate-700 mb-2;
+        }
+
+        input, textarea {
+            @apply shadow-sm appearance-none border w-full py-2 px-3 text-slate-700 leading-tight focus:outline-none rounded-sm
+        }
+
+        .error {
+            @apply text-red-500 text-sm 
+        }
+
+    </style>
+    {{-- blade-formatter-enable --}}
+
     @yield('styles')
 </head>
-<body>
-    <h1>@yield('title')</h1>
+<body class="container mx-auto mt-10 mb-10 max-w-lg">
+    <h1 class="mb-4 text-3xl">@yield('title')</h1>
     <div>
-        @if (session()->has('success'))
+        <!-- @if (session()->has('success'))
             <div>{{ session('success') }}</div>   
-        @endif
+        @endif -->
+
+        <div class="mb-10 rounded border border-green-400 bg-green-100 px-4 py-3 text-lg text-green-700">
+            <strong class="font-bold">Success!</strong>
+        <div>This is a flash message!</div>
+        </div>
         @yield('content')
     </div>
 </body>
