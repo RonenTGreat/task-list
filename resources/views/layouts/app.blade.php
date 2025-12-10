70f98162f93e96d2606a247f865a5096c3a62291
RonenTGreat <ronenh53@gmail.com>
2025-12-10 13:39:32 +0000

    added interactivity with alpinejs

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,10 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Laravel Task List App</title>
     <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+    <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
+
 
     {{-- blade-formatter-disable --}}
     <style type="text/tailwindcss">
@@ -33,18 +36,29 @@
 
     @yield('styles')
 </head>
+
 <body class="container mx-auto mt-10 mb-10 max-w-lg">
     <h1 class="mb-4 text-3xl">@yield('title')</h1>
-    <div>
-        <!-- @if (session()->has('success'))
-            <div>{{ session('success') }}</div>   
-        @endif -->
+    <div x-data="{flash: true}">
+        @if (session()->has('success'))
 
-        <div class="mb-10 rounded border border-green-400 bg-green-100 px-4 py-3 text-lg text-green-700">
+
+        <div x-show="flash" class="relative mb-10 rounded border border-green-400 bg-green-100 px-4 py-3 text-lg text-green-700"
+            role="alert">
             <strong class="font-bold">Success!</strong>
-        <div>This is a flash message!</div>
+            <div>{{ session('success') }}</div>
+
+            <span class="absolute top-0 bottom-0 right-0 px-4 py-3">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                    stroke-width="1.5" @click="flash = false"
+                    stroke="currentColor" class="h-6 w-6 cursor-pointer">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                </svg>
+            </span>
         </div>
+        @endif
         @yield('content')
     </div>
 </body>
+
 </html>
